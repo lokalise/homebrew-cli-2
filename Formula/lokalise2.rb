2b72011b2ba299e239974dec1b0b8dd890e955f0
Nick Ustinov <nickustinov@gmail.com>
2019-10-15 19:53:33 +0300

    2.00

--- Formula/lokalise2.rb
+++ Formula/lokalise2.rb
@@ -1,9 +1,9 @@
 class Lokalise2 < Formula
   desc "Lokalise CLI v2"
-  homepage "https://docs.lokalise.com/en/articles/3401683-lokalise-cli-v2"
+  homepage "https://docs.lokalise.com/cli2"
 
   version "2.00"
-  sha256 "59519884138bd158d92c5ba889af351e313cd902fad4630bec899d3d8f035340"
+  sha256 "fbdf5d7c2dd533bdd68ca29c2e88c3659a2239fc48a977c6aff3dacca860ec40"
 
   url "https://s3-eu-west-1.amazonaws.com/lokalise-assets/cli2/lokalise2-2.00-darwin-amd64.tgz"
 
