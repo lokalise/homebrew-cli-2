a05ac94ab500ea2f593b030942dc67f694c28dd1
Nick Ustinov <nickustinov@gmail.com>
2019-10-21 13:11:16 +0300

    2.01

--- Formula/lokalise2.rb
+++ Formula/lokalise2.rb
@@ -2,10 +2,10 @@ class Lokalise2 < Formula
   desc "Lokalise CLI v2"
   homepage "https://docs.lokalise.com/cli2"
 
-  version "2.00"
-  sha256 "6a28813c9c2f6186611a0eaa6db9ae9dbab5fd017e3bd6cdbab25a6deaac578c"
+  version "2.01"
+  sha256 "f172bbe72df6fc228c79e7a78ead3dc9bd19d85950256b882d1db6e525cc50e9"
 
-  url "https://s3-eu-west-1.amazonaws.com/lokalise-assets/cli2/lokalise2-2.00-darwin-amd64.tgz"
+  url "https://s3-eu-west-1.amazonaws.com/lokalise-assets/cli2/lokalise2-2.01-darwin-amd64.tgz"
 
   def install
     bin.install "lokalise2"
