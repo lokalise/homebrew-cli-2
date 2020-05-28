dc2164ff3ce93844a149550215359fea4214222a
goreleaserbot <goreleaser@carlosbecker.com>
2020-05-28 17:36:07 +0300

    Brew formula update for lokalise-cli-2-go version v2.5.1

--- Formula/lokalise2.rb
+++ Formula/lokalise2.rb
@@ -2,24 +2,24 @@
 class Lokalise2 < Formula
   desc "Lokalise CLI v2"
   homepage "https://docs.lokalise.com/cli2"
-  version "2.5.0"
+  version "2.5.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/lokalise/lokalise-cli-2-go/releases/download/v2.5.0/lokalise2_darwin_x86_64.tar.gz"
-    sha256 "6f1a3cca6aa09c905c284dc838f83393d1ed5e6807e0667313099cdfc3f23dda"
+    url "https://github.com/lokalise/lokalise-cli-2-go/releases/download/v2.5.1/lokalise2_darwin_x86_64.tar.gz"
+    sha256 "3f9a75f8efa640401fd050d09a238bad9d52f4e0355b8bccd6df2ad009236b78"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/lokalise/lokalise-cli-2-go/releases/download/v2.5.0/lokalise2_linux_x86_64.tar.gz"
-      sha256 "36bf678ee1356737e9f074fb45d5653d106d83848d369e0b54febfad6c99bb1e"
+      url "https://github.com/lokalise/lokalise-cli-2-go/releases/download/v2.5.1/lokalise2_linux_x86_64.tar.gz"
+      sha256 "030975d00ccd088f78939329ef5e5c268c77f4a531f10cfe1604ebd7da2c2afa"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/lokalise/lokalise-cli-2-go/releases/download/v2.5.0/lokalise2_linux_arm64.tar.gz"
-        sha256 "5b8d70c27cd39b45986c73081183fd58af24d6f8c55909dc5592637b2295eb07"
+        url "https://github.com/lokalise/lokalise-cli-2-go/releases/download/v2.5.1/lokalise2_linux_arm64.tar.gz"
+        sha256 "c0fb95bbde5b85ab78dd6cee66fe488830fb9a4e89d15a18c9f364d706e50615"
       else
-        url "https://github.com/lokalise/lokalise-cli-2-go/releases/download/v2.5.0/lokalise2_linux_armv6.tar.gz"
-        sha256 "45e53506e6d593d02d62ea66d676f7c7afd0eb2ffb585e57c989b293d7f3fa8c"
+        url "https://github.com/lokalise/lokalise-cli-2-go/releases/download/v2.5.1/lokalise2_linux_armv6.tar.gz"
+        sha256 "add2a480098009af294f3ad10950dc650c6f6fb5fe762996eded002cf983a5c3"
       end
     end
   end
